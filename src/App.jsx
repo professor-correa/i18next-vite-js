e8e9bf7383a60fc92b863257e8b93d936ee5dc25
lucascorreaa <lucas.lca14@gmail.com>
2025-11-11 15:13:50 -0300

    feat(app): integrate i18n into main component and add language switch buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useTranslation } from 'react-i18next'
 import './App.css'
+import "./i18n"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const {t, i18n} = useTranslation()
+  const changeLanguage = lang => i18n.changeLanguage(lang)
+  console.log(i18n.language)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>{t("welcome.title")}</h1>
+     <p>{t("welcome.description")} <a href="https://github.com/professor-correa/i18next-vite-js">{t("welcome.link")}</a></p>
+     <button onClick={() => changeLanguage("pt")}>pt</button>
+     <button onClick={() => changeLanguage("en")}>en</button>
     </>
   )
 }
